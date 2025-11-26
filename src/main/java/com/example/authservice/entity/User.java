ebddb7c6f4228c3875fbfa59285370d7bd9adc8d
Dimas Akmal <dimasawp13@gmail.com>
2025-11-26 23:31:25 +0700

    add: entity and repository for role and user

---
+++ src/main/java/com/example/authservice/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.authservice.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    private LocalDateTime createdAt;
+}
